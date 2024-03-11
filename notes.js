962ea60ac62f5bb0ac2a630809a12c7733c1df4c
Tamás Fodor <fodor.tamas48@gmail.com>
2024-03-11 19:50:53 +0100

    created add command

--- notes.js
+++ notes.js
@@ -1,1 +1,26 @@
-ECHO is on.
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+yargs(hideBin(process.argv))
+    .command({
+        command: 'add',
+        description: 'add new note',
+        builder: {
+            title: {
+                alias: 't',
+                description: 'note title',
+                demand: true,
+                type: 'string'
+            },
+            body: {
+                alias: 'b',
+                description: 'note body',
+                demand: true,
+                type: 'string'
+            }
+        },
+        handler: (argv) => {
+            console.log(`Add note: ${argv.title} ${argv.body}`);
+        }
+    })
+    .parse()
